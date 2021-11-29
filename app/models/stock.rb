ec3c2b64d6f0b3a6b5b99d073b62f6eb56e5598d
M. Brock Podgurski <mbpod10@gmail.com>
2021-11-29 13:19:20 -0500

    create stock list partial in search bar

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,4 +1,8 @@
 class Stock < ApplicationRecord
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
+  validates :name, :ticker, presence: true
 
   def self.new_lookup(ticker_symbol)
     client = IEX::Api::Client.new(
@@ -8,7 +12,7 @@ class Stock < ApplicationRecord
     )
     begin
       new(
-        ticker: ticker_symbol, 
+        ticker: ticker_symbol.upcase, 
         name: client.company(ticker_symbol).company_name, 
         last_price: client.price(ticker_symbol)
         )      
