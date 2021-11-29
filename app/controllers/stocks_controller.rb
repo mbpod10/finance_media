8a9b2f5a449b4fd03bfa36e37fd24de231e542d1
M. Brock Podgurski <mbpod10@gmail.com>
2021-11-29 12:25:40 -0500

    add search and error messages

--- app/controllers/stocks_controller.rb
+++ app/controllers/stocks_controller.rb
@@ -8,13 +8,18 @@ class StocksController < ApplicationController
           format.js { render partial: 'users/result' } 
         end        
       else
-        flash[:alert] = "Please enter a valid symbol to search"
-        redirect_to my_portfolio_path
+        respond_to do |format|         
+          flash.now[:alert] = "Invalid Symbol"
+          format.js { render partial: 'users/result' } 
+        end      
       end
 
     else
-      flash[:alert] = "Please Enter A Symbol"
-      redirect_to my_portfolio_path
+      respond_to do |format|         
+        flash.now[:alert] = "Please Enter A Symbol"
+        format.js { render partial: 'users/result' } 
+      end    
+
     end
   end
 end
